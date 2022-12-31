687fb561057e324f1ec2d41a30b3dbcc38ffd3c8
Mohibbull Munshi <33421746+Mohib04@users.noreply.github.com>
2022-12-31 11:23:59 +0600

    fix some error

--- task-6/index.php
+++ task-6/index.php
@@ -13,26 +13,35 @@
   <section class="container">
     <div class="header">
       <div class="display">
-        <?php 
-          if(isset($_POST['submit'])){
-            $number1 = $_POST['number1'];
-            $number2 = $_POST['number2'];
-            $operator = $_POST['operator'];
-            switch($operator){
-              case '+':
-                echo $number1+$number2;
-                break;
-              case '-':
-                echo $number1-$number2;
-                break;
-              case '/':
-                echo $number1/$number2;
-                break;
-              case '*':
-                echo $number1*$number2;
-                break;
-            }
+        <?php
+          if(is_numeric($_POST['number1']) && is_numeric($_POST['number2'])){
+              $number1 = $_POST['number1'];
+              $number2 = $_POST['number2'];
+              $operator = $_POST['operator'];
+                switch($operator){
+                  case '+':
+                    echo $number1+$number2;
+                    break;
+                  case '-':
+                    echo $number1-$number2;
+                    break;
+                  case '/':
+                    if($number2>0){
+                      echo $number1/$number2;
+                    }
+                    else{
+                      echo "Second Value should be greater than zero";
+                    }
+                    break;
+                  case '*':
+                    echo $number1*$number2;
+                    break;
+                }
           }
+          else{
+              echo "Please Enter Numeric Values";
+          }
+          
         ?>
       </div>
     </div>
@@ -40,9 +49,9 @@
       <div class="form">
         <form method="POST" action="#">
           <label for="number1">First Number</label>
-          <input id="number1" name=" number1" type="number">
+          <input id="number1" name=" number1" required type="text">
           <label for="number2">Second Number</label>
-          <input id="number2" name="number2" type="number">
+          <input id="number2" name="number2" required type="text">
           <select name="operator" id="">
             <option value="+">+</option>
             <option value="-">-</option>
